a1fd1561e3a56543c73eaa57f2ace31fe7a6a051
pechikonar <muthukonaruot@gmail.com>
2025-02-01 10:48:24 -0500

    Fixed the code for seed.sql to insert multiple rows of employees, roles and departments taking care of foreign key constraints

--- main/Develop/src/db/seed.sql
+++ main/Develop/src/db/seed.sql
@@ -1,37 +1,29 @@
 DO $$
 
-
 DECLARE
     var_dept_id 	INTEGER;
     var_role_id 	INTEGER;
     var_emp_id 	INTEGER;
 BEGIN
-    INSERT INTO department (name) 
-    VALUES ('Information Technology'),
-           ('Accounts and Finance'),
-           ('Marketing'),
-           ('Sales')
-    RETURNING id INTO var_dept_id;
+    INSERT INTO department (name) VALUES ('Information Technology') RETURNING id INTO var_dept_id;
+    INSERT INTO role (title, salary, department_id) VALUES ('Director', 255000.00, var_dept_id) RETURNING id INTO var_role_id;
+    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Muthu', 'Konar', var_role_id, NULL) RETURNING id INTO var_emp_id;
 
-    INSERT INTO role (title, salary, department_id)
-    VALUES 
-           ('Director', 255000.00, var_dept_id),
-           ('Senior Manager', 150000.00, var_dept_id),
-           ('Manager', 120000.00, var_dept_id),
-           ('Analyst', 100000.00, var_dept_id)
-    RETURNING id INTO var_role_id;
-    
-    INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES 
-           ('Muthu', 'Konar', var_role_id, NULL),
-           ('Peter', 'Sam', var_role_id, NULL),
-           ('Jon', 'Doe', var_role_id, NULL),
-           ('Sarah', 'Amir', var_role_id, NULL)
-    RETURNING id INTO var_emp_id;
+    INSERT INTO department (name) VALUES ('Accounts') RETURNING id INTO var_dept_id;
+    INSERT INTO role (title, salary, department_id) VALUES ('Manager', 155000.00, var_dept_id) RETURNING id INTO var_role_id;
+    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Priya', 'M', var_role_id, NULL) RETURNING id INTO var_emp_id;
    
-    COMMIT;
+    INSERT INTO department (name) VALUES ('Marketing') RETURNING id INTO var_dept_id;
+    INSERT INTO role (title, salary, department_id) VALUES ('Analyst', 120000.00, var_dept_id) RETURNING id INTO var_role_id;
+    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jon', 'Doe', var_role_id, NULL) RETURNING id INTO var_emp_id;
+
+    INSERT INTO department (name) VALUES ('Sales') RETURNING id INTO var_dept_id;
+    INSERT INTO role (title, salary, department_id) VALUES ('Officer', 130000.00, var_dept_id) RETURNING id INTO var_role_id;
+    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Peter', var_role_id, NULL) RETURNING id INTO var_emp_id;
 
 EXCEPTION WHEN OTHERS THEN
     ROLLBACK;
     RAISE NOTICE 'Error in inserts: %, %', SQLSTATE, SQLERRM;
 END $$;
+
+
