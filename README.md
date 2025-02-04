c5f42938bbc5c3d6c0dce9def12108a6ee3a121e
pechikonar <muthukonaruot@gmail.com>
2025-02-03 21:58:04 -0500

    README updated and walkthrough video link provided

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# EmployeeTracker
-Employee Tracker Project
+# Employee Tracker Project - Challenge 10
+
+## Description
+
+A command-line application from scratch to manage a company's employee database, using Node.js, Inquirer, and PostgreSQL.This application contains features like below 
+1. View all Departments
+2. View all Roles
+3. View all Employees
+4. Add a Department
+5. Add a Role
+6. Add an Employee
+7  Update an Employee Role
+
+
+
+## Installation 
+
+- Zip and download the starter code
+- ensure node.js and npm is installed
+- Open integrated terminal and execute npm install to install packages like/for inquirer, typescript etc.
+- execute npm run start
+
+
+
+## Usage and Video link for Employee Tracker
+
+Report generated to view all Deparments, all Roles and All Employees. Features available to add Department , Role and Employee. Additionally an Employee role can also be updated in case of change in Empoyees roles and responsibilities.
+
+Below is the video link on the Demo.
+https://drive.google.com/file/d/1JxPJKHiIcJ68ANqc7sPW2lWxgMryJfOj/view?usp=drive_link
+
+
+
+## Credits 
+- Instructor and TA's
+
+
+
+## Questions 
+For any questions, please contact me at [pmkonar@gmail.com](mailto:pmkonar@gmail.com).
+You can also find more of my work at [muthukonar](https://github.com/muthukonar).
+
--- main/Develop/src/db/seed.sql
+++ main/Develop/src/db/seed.sql
@@ -11,15 +11,15 @@ BEGIN
 
     INSERT INTO department (name) VALUES ('Accounts') RETURNING id INTO var_dept_id;
     INSERT INTO role (title, salary, department_id) VALUES ('Manager', 155000.00, var_dept_id) RETURNING id INTO var_role_id;
-    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Priya', 'M', var_role_id, NULL) RETURNING id INTO var_emp_id;
+    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Priya', 'Pechi', var_role_id, NULL) RETURNING id INTO var_emp_id;
    
     INSERT INTO department (name) VALUES ('Marketing') RETURNING id INTO var_dept_id;
     INSERT INTO role (title, salary, department_id) VALUES ('Analyst', 120000.00, var_dept_id) RETURNING id INTO var_role_id;
-    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jon', 'Doe', var_role_id, NULL) RETURNING id INTO var_emp_id;
+    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Anshu', 'Shivu', var_role_id, NULL) RETURNING id INTO var_emp_id;
 
     INSERT INTO department (name) VALUES ('Sales') RETURNING id INTO var_dept_id;
     INSERT INTO role (title, salary, department_id) VALUES ('Officer', 130000.00, var_dept_id) RETURNING id INTO var_role_id;
-    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Peter', var_role_id, NULL) RETURNING id INTO var_emp_id;
+    INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Siddha', 'Rishi', var_role_id, NULL) RETURNING id INTO var_emp_id;
 
 EXCEPTION WHEN OTHERS THEN
     ROLLBACK;
